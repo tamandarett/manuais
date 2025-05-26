623b680693ab608e8abb45e5fe470c23c76dfaa8
tamandarett <junior@tamandaretintas.com.br>
2025-05-26 13:55:20 -0300

    Update password.js

--- js/password.js
+++ js/password.js
@@ -1,2 +1,10 @@
-// SENHA: "tamandare123" (troque para a que você quiser)
-const SENHA_CORRETA = "tamandare123";
+function verificarSenha() {
+    const senhaDigitada = document.getElementById('senha').value;
+    const SENHA_CORRETA = "tamandare123"; // Troque aqui!
+    
+    if (senhaDigitada === SENHA_CORRETA) {
+        window.location.href = "manuais/";
+    } else {
+        document.getElementById('erro').textContent = "Senha incorreta!";
+    }
+}
